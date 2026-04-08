f4710d4d0c74ec0ebb9cb4958b9ce8e02cd739eb
Akash Chowdhury <emailidakashchowdhury@gmail.com>
2026-04-08 21:37:48 +0530

    update in testingggg

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -36,7 +36,7 @@ function Hero() {
     <div className="section-container">
       <div className="hero-section">
         <div className="text-section">
-          <p className="text-headline">❤️ Health comes first of all all</p>
+          <p className="text-headline">❤️ Health comes first of all</p>
           <h2 className="text-title">
             Find your Doctor and make an Appointments
           </h2>

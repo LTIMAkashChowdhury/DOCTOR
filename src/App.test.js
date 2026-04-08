a4be7e7b860aa2d46ff204b62312e7cc7919eb07
Akash Chowdhury <emailidakashchowdhury@gmail.com>
2026-04-08 12:41:52 +0530

    check

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders app heading', () => {
+test('renders app title', () => {
   render(<App />);
   const element = screen.getByText(/react/i);
   expect(element).toBeInTheDocument();
 });
-

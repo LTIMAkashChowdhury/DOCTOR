ca9f5523739c3ef1fdf7145fa24dfe8986f6a85c
LTIMAkashChowdhury <akash.10849154@ltimindtree.com>
2026-04-17 15:37:03 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,7 +15,7 @@ function About() {
           <span>About Us</span>
         </h3>
         <p className="about-description">
-          Welcome to Health Plus, your trusted partner for accessible and testingggggg
+          Welcome to Health Plus, your trusted partner for accessible and testingggggg now
           personalized healthcare. Our expert doctors offer online consultationssss now lets see
           and specialized services, prioritizing your well-being. Join us onon okay
           this journey towards a healthier you.
